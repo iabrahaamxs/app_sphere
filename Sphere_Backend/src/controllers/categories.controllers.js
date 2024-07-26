0527fb16d38bf68118b3246460c882095b19762c
Abraham <1001.28021547.ucla@gmail.com>
2024-07-26 00:22:41 -0400

    functions categories

--- Sphere_Backend/src/controllers/categories.controllers.js
+++ Sphere_Backend/src/controllers/categories.controllers.js
@@ -1,14 +1,22 @@
 import { poll } from "../db.js";
+import { CategorieModel } from "../models/categories.models.js";
 
-export const createUserCategorie = async (req, res) => {
-    
+const createUserCategorie = async (req, res) => {
   try {
     const data = req.body;
-    
-    const { rows } = await poll.query(
-      "INSERT INTO users_categories (u_c_user, categorie) VALUES ($1, $2) RETURNING *",
-      [data.user, data.categorie]
-    );
+    const rows = [];
+
+    if (data.categories < 1) {
+      return res.json({ menssage: "User without categories" });
+    }
+
+    for (let index = 0; index < data.categories.length; index++) {
+      const categorie = await CategorieModel.create(
+        data.user_id,
+        data.categories[index]
+      );
+      rows[index] = categorie;
+    }
 
     return res.json(rows);
   } catch (error) {
@@ -16,16 +24,49 @@ export const createUserCategorie = async (req, res) => {
   }
 };
 
-export const getCategories = async (req, res) => {
+const getCategories = async (req, res) => {
   const { id } = req.params;
-  const { rows } = await poll.query("SELECT * FROM users_categories WHERE u_c_user = $1", [
-    id,
-  ]);
 
-  if (rows.length === 0) {
+  const categories = await CategorieModel.getCategories(id);
+
+  if (categories.length === 0) {
     return res.status(404).json({ menssage: "User without categories" });
   }
 
-  res.json(rows);
+  res.json(categories);
 };
 
+const updateCategories = async (req, res) => {
+  try {
+    const data = req.body;
+
+    // insertar categorias si no existen
+    for (let i = 0; i < data.categoriesOn.length; i++) {
+      const categorie = await CategorieModel.findCategorie(
+        data.user_id,
+        data.categoriesOn[i]
+      );
+      if (!categorie) {
+        CategorieModel.create(data.user_id, data.categoriesOn[i]);
+      }
+    }
+
+    // eliminar categorias si existen
+    for (let i = 0; i < data.categoriesOff.length; i++) {
+      CategorieModel.update(data.user_id, data.categoriesOff[i]);
+    }
+
+    const categories = await CategorieModel.getCategories(data.user_id);
+
+    return res.json(categories);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ menssage: "Internal server error" });
+  }
+};
+
+export const CategorieController = {
+  createUserCategorie,
+  getCategories,
+  updateCategories,
+};
