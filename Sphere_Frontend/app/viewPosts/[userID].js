5113f54cbaa6f952efebf4d4803986677fdcd3b4
Abraham <1001.28021547.ucla@gmail.com>
2024-10-23 21:55:22 -0400

    check

--- Sphere_Frontend/app/viewPosts/[userID].js
+++ Sphere_Frontend/app/viewPosts/[userID].js
@@ -1,20 +1,22 @@
 import { Stack } from "expo-router";
 import { FlatList, Text, View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { PostApi } from "../../api/postsApi";
+import Post from "../../components/Post";
 
 export default function PostsUser() {
   const { userID } = useLocalSearchParams();
-
   const [posts, setPosts] = useState([]);
+  const [view, setView] = useState(false);
+  const ref = useRef(null);
 
   const fetchData = async () => {
     if (userID) {
       const postsData = await PostApi.getPosts(userID);
-      console.log("id", userID);
 
       setPosts(postsData);
+      setView(true);
     }
   };
 
@@ -22,25 +24,29 @@ export default function PostsUser() {
     fetchData();
   }, [userID]);
 
+  if (!view) {
+    return;
+  }
+
   return (
     <View>
       <Stack.Screen
         options={{
           headerShown: true,
           headerStyle: {},
-          headerTitle: `${"mesi"}`,
+          headerTitle: `${posts[0].name}`,
           headerShadowVisible: false,
         }}
       />
-      {/* <FlatList
-        data={filteredPosts}
+      <FlatList
+        data={posts}
         renderItem={({ item }) => (
           <View>
             <Post item={item} />
           </View>
         )}
-        keyExtractor={(item, index) => index}
-      /> */}
+        keyExtractor={(item) => item.post_id}
+      />
       <Text>{userID}</Text>
     </View>
   );
