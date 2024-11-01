691cc0aa3df53db5fc41b3770eed99339bd701f2
Abraham <1001.28021547.ucla@gmail.com>
2024-10-31 21:48:17 -0400

    checkpoint

--- Sphere_Frontend/app/viewPosts/[userID].js
+++ Sphere_Frontend/app/viewPosts/[userID].js
@@ -7,25 +7,33 @@ import Post from "../../components/Post";
 
 export default function PostsUser() {
   const { userID } = useLocalSearchParams();
+  const [id, index] = userID.split("%"); // Separa id e índice
   const [posts, setPosts] = useState([]);
   const [view, setView] = useState(false);
   const ref = useRef(null);
 
   const fetchData = async () => {
-    if (userID) {
-      const postsData = await PostApi.getPosts(userID);
-
+    if (id) {
+      const postsData = await PostApi.getPosts(id);
       setPosts(postsData);
       setView(true);
     }
   };
 
   useEffect(() => {
-    fetchData();
-  }, [userID]);
+    fetchData(); // Carga los datos de los posts
+  }, [id]); // Dependencia de `id`
+
+  useEffect(() => {
+    if (ref.current && index != null && posts.length > 0) {
+      ref.current.scrollToIndex({ index: parseInt(index, 10), animated: true });
+    }
+    console.log("troll");
+    
+  });
 
   if (!view) {
-    return;
+    return null;
   }
 
   return (
@@ -34,11 +42,12 @@ export default function PostsUser() {
         options={{
           headerShown: true,
           headerStyle: {},
-          headerTitle: `${posts[0].name}`,
+          headerTitle: `${posts[0]?.name || "Posts"}`,
           headerShadowVisible: false,
         }}
       />
       <FlatList
+        ref={ref}
         data={posts}
         renderItem={({ item }) => (
           <View>
@@ -46,6 +55,12 @@ export default function PostsUser() {
           </View>
         )}
         keyExtractor={(item) => item.post_id}
+        
+        onScrollToIndexFailed={(info) => {
+          setTimeout(() => {
+            ref.current?.scrollToIndex({ index: info.index, animated: true });
+          }, 500);
+        }}
       />
     </View>
   );
