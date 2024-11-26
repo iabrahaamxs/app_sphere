9fd721eb99e2bce3a928ed26e935af46c9a9e16a
Abraham <1001.28021547.ucla@gmail.com>
2024-11-26 00:02:20 -0400

    expo public url base .env

--- Sphere_Frontend/api/apiManager.js
+++ Sphere_Frontend/api/apiManager.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+console.log(process.env.EXPO_PUBLIC_API_URL_BASE); 
+
 const axiosManager = axios.create({
-  baseURL: process.env.API_URL_BASE,
+  baseURL: process.env.EXPO_PUBLIC_API_URL_BASE,
   //"https://app-sphere.onrender.com",
   //responseType: 'json',
   //withCredentials: true
